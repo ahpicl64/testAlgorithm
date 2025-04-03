87c0206d2c5eb49458f69d70449a9f48e72b052d
임재홍 <ahpicl64@gmail.com>
2025-04-03 11:12:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,46 +1,42 @@
 from collections import deque
-import queue
 import sys
 
 
 input = sys.stdin.readline
-n = int(input())
+n = int(input().strip())
 
-graph = [list(map(int, list(input().strip()))) for _ in range(n)]
+graph = [list(map(int,list(input().strip()))) for _ in range(n)]
+visit = [[False] * n for _ in range(n)] 
 result = []
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-visit = [[False] * n for _ in range(n)]
 
-# 단지를 찾음
-def bfs(x,y):
-    queue = deque([(x,y)])
+
+def bfs(y,x):
     if visit[y][x]:
         return
-    # 단지 수 판정용 카운터
+    queue = deque([(y,x)])
     visit[y][x] = True
     count = 1
-    
-    while queue: # 큐 다 빠질때까지
-    # 이웃 노드 순회할 때 마다 카운트 + 1
-        x,y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and not visit[ny][nx] and graph[ny][nx] == 1:
-                visit[ny][nx] = True
-                queue.append([nx,ny])
-                count += 1
+    while queue:
+        curr_y, curr_x = queue.popleft()
+        # print(queue)
 
-    # 카운트 된 집을 결과 리스트에 등록
+        for dy,dx in [[1,0],[-1,0],[0,1],[0,-1]]:
+            ny, nx = curr_y + dy, curr_x + dx
+            if 0 <= ny < n and 0 <= nx < n and not visit[ny][nx] and graph[ny][nx] == 1:
+                visit[ny][nx] = True
+                queue.append((ny,nx))
+                count +=1
+
+
     result.append(count)
+
 
 for y in range(n):
     for x in range(n):
-        if graph[y][x] == 1 and not visit[y][x]:
-            bfs(x,y)
+        if not visit[y][x] and graph[y][x] == 1:
+            bfs(y,x)
 
 result.sort()
 print(len(result))
-for i in range(len(result)):
+for i in range(len(result)):   
     print(result[i])
